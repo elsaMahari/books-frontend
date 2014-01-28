ca1caa94264b91fe6d1a58a148b2e9ad2bdc0eaf
ema <elsa.mahari@gmx.de>
2014-01-29 00:12:55 +0100

    submit basic form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('booksFrontendApp')
-  .controller('MainCtrl', function ($scope,$http) {
-    $http({method: 'GET', url: '/books'}).success(function(data, status, headers, config){
-   	  $scope.booksAsJson = data;
-    });
-  });
+.controller('MainCtrl', function ($scope,$http) {
+	$http({method: 'GET', url: '/books'}).success(function(data, status, headers, config){
+		$scope.booksAsJson = data;
+	});
+	$scope.list = [];
+	$scope.text = 'hello';
+	$scope.submit = function() {
+		if (this.text) {
+			this.list.push(this.text);
+			this.text = '';
+		}
+	};
+});
+
